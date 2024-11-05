711960e1285d3f60b98a81df111398b93ec1a745
Kushagra Goyal <kushagra.goyal@infoneotech.com>
2024-11-05 15:51:39 +0530

    user data store into store

--- ticket2/api/auth.js
+++ ticket2/api/auth.js
@@ -6,7 +6,18 @@ export const registerUser = async (userData) => {
       return response.data;
     } catch (error) {
         console.log(error);
-        
+
       throw new Error(error.response?.data?.message || 'Registration failed');
+    }
+};
+
+export const loginUser = async (userData) => {
+    try {
+      const response = await api.post('/api/v1/auth/login', userData);
+      return response.data;
+    } catch (error) {
+        console.log(error);
+
+      throw new Error(error.response?.data?.message || 'Login failed');
     }
 };
--- ticket2/src/store/slices/authSlice.js
+++ ticket2/src/store/slices/authSlice.js
@@ -1,19 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getFromLocalStorage, removeFromLocalStorage, saveToLocalStorage } from "../../../utils/storage";
 
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
-    user: null,
-    token: null,
+    user: getFromLocalStorage('authData') || {},
+    token: getFromLocalStorage('authToken') || null,
   },
   reducers: {
     setUser: (state, action) => {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
+      state.user = action?.payload?.user;
+      state.token = action?.payload?.token;
+      saveToLocalStorage('authData', action.payload.user)
+      saveToLocalStorage('authToken', action.payload.token)
     },
     clearUser: (state) => {
       state.user = null;
       state.token = null;
+      removeFromLocalStorage('authData');
+      removeFromLocalStorage('authToken');
     },
   },
 });
