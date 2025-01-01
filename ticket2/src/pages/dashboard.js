470daa624fffabbbff1467efddf82fb329055428
Sahil Goyal <88875321+ketangoyal1042@users.noreply.github.com>
2025-01-01 17:21:39 +0530

    Create New Event

--- ticket2/src/pages/dashboard.js
+++ ticket2/src/pages/dashboard.js
@@ -1,12 +1,31 @@
-import { Button } from "@mui/material";
-import React from "react";
+import WrapperModal from "@/components/common/WrapperModal";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import CreateEvent from "@/components/forms/CreateEvent";
+import TopFiveEvents from "@/components/TopFiveEvents";
 
 const dashboard = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <h2>Welcome to your dashboard!</h2>
-      <Button variant="outlined">Outlined</Button>
+    <div className="bg-gray-50 py-24 sm:py-32">
+      <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8 text-center">
+        <Button
+          className="text-center text-base/7 font-semibold text-indigo-600"
+          onClick={handleOpen}
+        >
+          Create New Event
+        </Button>
+        <p className="mx-auto mt-2 max-w-lg text-balance text-center text-3xl font-semibold tracking-tight text-gray-950 sm:text-5xl">
+          Everything you need to deploy your app
+        </p>
+        <Button onClick={handleOpen}>Open modal</Button>
+        <WrapperModal open={open} setOpen={setOpen}>
+          <CreateEvent />
+        </WrapperModal>
+        <TopFiveEvents />
+      </div>
     </div>
   );
 };
