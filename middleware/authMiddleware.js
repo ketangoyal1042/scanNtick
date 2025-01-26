dfa084c091c14c9af8b17fe9ddd23e8b7b07ad72
Kushagra Goyal <kushagragoyal1032@gmail.com>
2025-01-26 18:37:34 +0530

    visitor tickets

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -19,6 +19,24 @@ export const requireSignIn = async (req, res, next) => {
   }
 };
 
+export const requireVisitorSignIn = async (req, res, next) => {
+  try {
+    const decode = JWT.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    req.visitor = decode;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Encounter Error while in Authorization",
+      error,
+    });
+  }
+};
+
 export const checkEventAdministrator = async (req, res, next) => {
   try {
     const { _id: id } = req.user;
--- routes/ticketRoute.js
+++ routes/ticketRoute.js
@@ -2,13 +2,15 @@ import express from "express";
 import {
   scanQrController,
   ticketGeneratorContoller,
+  ticketListController,
 } from "../controller/ticketController.js";
-import { requireSignIn } from "../middleware/authMiddleware.js";
+import { requireSignIn, requireVisitorSignIn } from "../middleware/authMiddleware.js";
 import { accociateHost } from "../middleware/eventMiddleware.js";
 
 const router = express.Router();
 
 router.post("/generate-ticket", requireSignIn, ticketGeneratorContoller);
 router.get("/scan", requireSignIn, accociateHost, scanQrController);
+router.post("/ticketList", ticketListController);
 
 export default router;
